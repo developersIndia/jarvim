d2ff76e2effef1688c3b71dd6d460c55e3af52c2
Jarmos-san <somraj.mle@gmail.com>
2021-09-21 15:56:54 +0530

    :hammer: Clean up the bootstrap scripts

--- scripts/Get-Jarvim.ps1
+++ scripts/Get-Jarvim.ps1
@@ -1,14 +1,25 @@
+<#
+TODO: Add documentations
+TODO: Make the first release & configure the script to download from there
+TODO: Refactor & simplify the execution flow, if possible get rid of using Git
+#>
+
+$JarvimDir = Resolve-Path -Path "$ENV:USERPROFILE\Downloads\Jarvim"
+
 try {
-    git | Out-Null
+  git | Out-Null
 }
 catch [System.Management.Automation.CommandNotFoundException] {
-    Write-Error "Git is either not installed or not available at $PATH. Please ensure it's installed & configured"
+  Write-Error "Git is either not installed or not available at $PATH. Please ensure it's installed & configured"
 }
 
-if ( Test-Path -Path $ENV:USERPROFILE\Downloads\Jarvim ) {
-    Write-Host "It appears a folder named Jarvim already exists, not downloading it again"
-} else {
-    New-Item -Path "$ENV:USERPROFILE\Downloads" -Name "Jarvim" -ItemType "directory" -Force | Out-Null
-    Invoke-Expression "git clone --quiet https://github.com/Jarmos-san/jarvim.git $ENV:USERPROFILE\Downloads\Jarvim"
-    Write-Host "Downloading the necessary files to $ENV:USERPROFILE\Downloads\Jarvim"
+if ( Test-Path -Path $JarvimDir ) {
+  Write-Host "Updating Jarvim with the latest changes & commits"
+  Set-Location -Path $JarvimDir
+  Invoke-Expression -Command "git pull --quiet"
+}
+else {
+  New-Item -Path "$ENV:USERPROFILE\Downloads" -Name "Jarvim" -ItemType "directory" -Force | Out-Null
+  Invoke-Expression -Command "git clone --quiet https://github.com/Jarmos-san/jarvim.git $JarvimDir"
+  Write-Host "Downloading the necessary files to $JarvimDir"
 }
