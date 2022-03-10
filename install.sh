5a1b2d3cdd5d8bb01a8e7140375e025643fd8a6c
Somraj Saha <somraj.1994@gmail.com>
2022-03-10 17:48:48 +0530

    :construction_worker: Update installation script to download the latest release

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env sh
 #
-#############################
+###############################################################################
 # Script to install Jarvim
-#############################
+###############################################################################
 #
-#############################
+###############################################################################
 # License: MIT License
 # Author:  Somraj Saha
-#############################
+###############################################################################
 
 set -e pipefail
 
-echo "Hello, World!"
+###############################################################################
+# Get the URL to download the latest tarball from a GitHub release
+###############################################################################
+URL_ENDPOINT="https://api.github.com/repos/Jarmos-san/Jarvim/releases/latest"
+DOWNLOAD_URL=$(curl -fsSL $URL_ENDPOINT \
+    | grep "browser_download_url" \
+    | cut -d '"' -f 4)
+
+# echo $DOWNLOAD_URL
+curl -fsLO $DOWNLOAD_URL
