db52b7fff65e4b4ce99fbbc451fc7d02c4040947
Somraj Saha <somraj.1994@gmail.com>
2022-03-14 17:11:38 +0530

    :pencil2: Leave a draft set of comments for future reference

--- scripts/check-dependencies.sh
+++ scripts/check-dependencies.sh
@@ -8,3 +8,12 @@
 # License: MIT License
 # Author:  Somraj Saha
 ###############################################################################
+
+# TODO: Run "has" command to check for availability of dependencies
+#* Ref: https://github.com/kdabir/has
+
+#! The following needs to exists for the project to work well
+#     - curl
+#     - tar
+#     - neovim
+#     - git
