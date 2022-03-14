7943d754596c3659badeb247ca5f29a50b92f3ac
Somraj Saha <somraj.1994@gmail.com>
2022-03-14 15:22:12 +0530

    :recycle: Clean up the installation script to be bit more verbose

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@
 
 set -e pipefail
 
-###############################################################################
-# Get the URL to download the latest tarball from a GitHub release
-###############################################################################
+# URL for info about the latest release of the project
 URL_ENDPOINT="https://api.github.com/repos/Jarmos-san/Jarvim/releases/latest"
-DOWNLOAD_URL=$(curl -fsSL $URL_ENDPOINT \
+
+# Download URL for the gzipped file available in the latest release
+DOWNLOAD_URL=$(curl --fail --silent --show-error --location $URL_ENDPOINT \
     | grep "browser_download_url" \
     | cut -d '"' -f 4)
 
 # echo $DOWNLOAD_URL
-curl -fsLO $DOWNLOAD_URL
+curl --fail --silent --location --remote-name $DOWNLOAD_URL
